0536426015ae4265f34875bd89cebf57030c3692
FedorovOlegUS <FedorovOlUS@gmail.com>
2023-04-10 01:54:20 -0400

    Fix 10101

--- src/test/java/first/First_Test.java
+++ src/test/java/first/First_Test.java
@@ -15,31 +15,8 @@ public class First_Test {
 
         System.out.println("Простой текст для проверки фиксов в гите.");
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        // Данные опции важны для remote тестов, так как без них ничего не запустится
+        String value
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        String title = driver.getTitle();
-        Assert.assertEquals("Web form", title);
-
-        Thread.sleep(2000);
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-        WebElement message = driver.findElement(By.id("message"));
-        String value = message.getText();
-        Assert.assertEquals("Received!", value);
-
-        driver.quit();
-    }
-
-}
 
 
