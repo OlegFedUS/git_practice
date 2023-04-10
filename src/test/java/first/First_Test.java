03730b714a7f5ff7dde4469936b2f3cbacdd819a
FedorovOlegUS <FedorovOlUS@gmail.com>
2023-04-10 00:51:26 -0400

    main

---
+++ src/test/java/first/First_Test.java
@@ -0,0 +1,43 @@
+package first;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class First_Test {
+
+    @Test
+    public void testSecond() throws InterruptedException {
+
+        //ChromeOptions chromeOptions = new ChromeOptions();
+        //chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        // Данные опции важны для remote тестов, так как без них ничего не запустится
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("Web form", title);
+
+        Thread.sleep(2000);
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        Assert.assertEquals("Received!", value);
+
+        driver.quit();
+    }
+
+}
+
+
